c7700e4a7a16bf4496a1400fa2f86058ccea0424
Rildo Demarqui <rildo17@gmail.com>
2019-09-16 18:56:23 -0300

    Create 06.00 printTable.py

---
+++ Automate the Boring Stuff with Python/06.00 printTable.py
@@ -0,0 +1,20 @@
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+colWidths = [0] * len(tableData)
+
+# Discovering the column width
+for item in range(len(tableData)):
+    colWidths[item] = len(max(tableData[item], key=len))
+
+# Printing the result
+for column in range(len(tableData[0])):
+    for line in range(len(tableData)):
+        print(tableData[line][column].rjust(colWidths[line]), end = ' ')
+        
+    print('')
+
+        
+        
+        
