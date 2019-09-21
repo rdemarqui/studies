405cbf3c78f4af77fec073eb436cfe5a7bbde79a
Rildo Demarqui <rildo17@gmail.com>
2019-09-21 10:23:12 -0300

    Create 07.01 robust password detection.py

---
+++ Automate the Boring Stuff with Python/07.01 robust password detection.py
@@ -0,0 +1,26 @@
+#! python
+# Script to detect a robust password
+
+import re
+
+# password input
+
+def checkpass(password):
+    # Eight characters, upcase and lowercase, one digit
+    # Creating patterns
+    eightChar = re.compile(r'\d{8}|\w{8}')   # Eight characters
+    lowerChar = re.compile(r'[a-z]')         # Lowercase
+    upperChar = re.compile(r'[A-Z]')         # Uppercase
+    digitChar = re.compile(r'\d')            # One digit
+    
+    # Validation
+    if eightChar.search(password) == None:
+        print('Your password must have at least 8 characters')
+    elif lowerChar.search(password) == None:
+        print('Your password must have lowercase characters')
+    elif upperChar.search(password) == None:
+        print('Your password must have uppercase characters')
+    elif digitChar.search(password) == None:
+        print('Your password must have at least 1 digit')
+    else:
+        print('Your password is safe')
